a1ea6ceaa99ffb12c83b7818ec8a1ba19babf68e
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-21 16:30:01 -0700

    all routes working

--- routes/posts.js
+++ routes/posts.js
@@ -14,8 +14,8 @@ router.get("/", async (req, res) => {
 });
 
 // GET ONE post
-router.get("/:id", (req, res) => {
-  res.send("GET ONE test");
+router.get("/:id", getPost, (req, res) => {
+  res.json(res.post);
 });
 
 // CREATE post
@@ -34,13 +34,47 @@ router.post("/", async (req, res) => {
 });
 
 // UPDATE post
-router.patch("/:id", (req, res) => {
-  console.log("GET ALL TEST");
+router.patch("/:id", getPost, async (req, res) => {
+  if (req.body.user != null) {
+    res.post.user = req.body.user;
+  }
+  if (req.body.message != null) {
+    res.post.message = req.body.message;
+  }
+  if (req.body.category != null) {
+    res.post.category = req.body.category;
+  }
+  try {
+    const updatedPost = await res.post.save();
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // DELETE POST
-router.delete("/:id", (req, res) => {
-  console.log("GET ALL TEST");
+router.delete("/:id", getPost, async (req, res) => {
+  try {
+    await res.post.remove();
+    res.json({ message: "Deleted post." });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+// Middleware
+async function getPost(req, res, next) {
+  let post;
+  try {
+    post = await Post.findById(req.params.id);
+    if (post == null) {
+      return res.status(404).json({ message: "Cannot find post." });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  res.post = post;
+  next();
+}
 
 module.exports = router;
